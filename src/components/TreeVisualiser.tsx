f3e12db8bb147653662452e6bd5fd99a181ad509
naman360 <naman.geek@gmail.com>
2024-07-30 22:59:01 +0530

    feat: basic canvas setup'

--- src/components/TreeVisualiser.tsx
+++ src/components/TreeVisualiser.tsx
@@ -1,9 +1,16 @@
-type Props = {};
+import { useEffect, useRef } from "react";
 
-const TreeVisualiser = (props: Props) => {
+const TreeVisualiser = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    canvasRef.current!.width = window.innerWidth;
+    canvasRef.current!.height = window.innerHeight;
+  }, []);
+
   return (
     <div>
-      <canvas />
+      <canvas ref={canvasRef} />
     </div>
   );
 };
