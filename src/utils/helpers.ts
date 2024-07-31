808a3f9f51c571ac107109f34b6928fc75dab301
naman360 <naman.geek@gmail.com>
2024-07-31 08:21:26 +0530

    feat: draw node functionality implemented

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,12 +1,36 @@
-import { Node } from "./binary-tree";
+import { BinaryTree, Node } from "./binary-tree";
 import { DRAWING_CONFIG } from "./constants";
 
 const getActualTreeDimensions = (root: Node) => {
   const treeActualHeight =
     root.getTreeHeight() * DRAWING_CONFIG.nodeVerticalSpace;
   const treeActualWidth =
-    root.getTreeHeight() * DRAWING_CONFIG.nodeHorizontalSpace;
+    Math.pow(2, root.getTreeHeight()) * DRAWING_CONFIG.nodeHorizontalSpace;
   return { treeActualHeight, treeActualWidth };
 };
 
-export { getActualTreeDimensions };
+const drawNode = (
+  node: BinaryTree,
+  canvas: HTMLCanvasElement | null,
+  arcX: number,
+  arcY: number
+) => {
+  const context = canvas?.getContext("2d");
+  if (!context) return;
+  context.beginPath();
+  context.arc(arcX, arcY, DRAWING_CONFIG.circleRadius, 0, 2 * Math.PI);
+  context.fillStyle = "#90EE90";
+  context.fill();
+};
+
+const drawTree = (
+  root: BinaryTree,
+  canvas: HTMLCanvasElement | null,
+  startX: number,
+  endX: number
+) => {
+  const arcCenterX = (startX + endX) / 2;
+  const arcCenterY = DRAWING_CONFIG.nodeVerticalSpace / 2;
+  drawNode(root, canvas, arcCenterX, arcCenterY);
+};
+export { getActualTreeDimensions, drawTree, drawNode };
